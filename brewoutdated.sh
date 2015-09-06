b4bc8e7b1243c42f27f0c56fab5f969fc067e0aa
christheswiss <chris.luscher@gmx.de>
2015-09-06 23:57:32 +0200

    more info

--- brewoutdated.sh
+++ brewoutdated.sh
@@ -2,4 +2,4 @@
 
 /usr/local/bin/brew update;
 outdated="$(/usr/local/bin/brew outdated)";
-test "${outdated}" != "" && echo "==> Outdated Formulae" && echo "${outdated}" && /usr/local/bin/terminal-notifier -title "Homebrew Update" -message "Packages require update";
+test "${outdated}" != "" && echo "==> Outdated Formulae" && echo "${outdated}" && /usr/local/bin/terminal-notifier -title "Homebrew Update" -message "Packages require update: ${outdated}";
