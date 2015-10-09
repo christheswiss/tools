a7e00e2f6bd526c5d359897e7581c9f09ce0f765
christheswiss <chris.luscher@gmx.de>
2015-10-10 00:30:54 +0200

    fixes bracket

--- brewoutdated.sh
+++ brewoutdated.sh
@@ -3,4 +3,4 @@
 /usr/local/bin/brew update;
 outdated="$(/usr/local/bin/brew outdated)";
 test "${outdated}" != "" && echo "==> Outdated Formulae" && echo "${outdated}" && /usr/local/bin/terminal-notifier -title "Homebrew" -message "Packages require update: ${outdated}";
-test "$(outdated)" == "" && /usr/local/bin/terminal-notifier -title "Homebrew" -message "No update required";
+test "${outdated}" == "" && /usr/local/bin/terminal-notifier -title "Homebrew" -message "No update required";
