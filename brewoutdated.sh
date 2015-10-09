9d27e6052b36ce314c43ddf7dd707c582258939a
christheswiss <chris.luscher@gmx.de>
2015-10-09 18:05:34 +0200

    adds output

--- brewoutdated.sh
+++ brewoutdated.sh
@@ -2,4 +2,5 @@
 
 /usr/local/bin/brew update;
 outdated="$(/usr/local/bin/brew outdated)";
-test "${outdated}" != "" && echo "==> Outdated Formulae" && echo "${outdated}" && /usr/local/bin/terminal-notifier -title "Homebrew Update" -message "Packages require update: ${outdated}";
+test "${outdated}" != "" && echo "==> Outdated Formulae" && echo "${outdated}" && /usr/local/bin/terminal-notifier -title "Homebrew" -message "Packages require update: ${outdated}";
+test "$(outdated)" == "" && /usr/local/bin/terminal-notifier -title "Homebrew" -message "No update required";
