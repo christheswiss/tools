2993b050827dc83a5c530ace2e8532002ba3f52e
christheswiss <chris.luscher@gmx.de>
2016-06-13 10:31:18 +0200

    adds caskroom dir cleanup

---
+++ caskroom-cleanup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dir=()
+
+for j in /usr/local/Caskroom/*
+do
+    dir+=($j)
+    for k in "$j"/*
+    do
+        dir+=($k)
+    done
+done
+
+dirnum=${#dir[@]}
+for i in $(seq $dirnum 1)
+do
+if [ "$(ls -A ${dir[$i-1]})" ]; then
+    echo "Not Empty:" ${dir[$i-1]}
+else
+    echo "Deleting:" ${dir[$i-1]}
+    rmdir ${dir[$i-1]}
+fi
+done
