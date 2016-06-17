dccc1f772336a1fa2526319b7e1ab9e05ba61df1
christheswiss <chris.luscher@gmx.de>
2016-06-17 17:43:07 +0200

    adds check if dir exists & del .DS_Store

--- series-cleanup.sh
+++ series-cleanup.sh
@@ -7,17 +7,26 @@ do
     dir+=($j)
     for k in "$j"/*
     do
-        dir+=($k)
+        if [ -d $k ]
+        then
+            dir+=($k)
+        fi
     done
 done
 
 dirnum=${#dir[@]}
+
 for i in $(seq $dirnum 1)
 do
-if [ "$(ls -A ${dir[$i-1]})" ]; then
-    echo "Not Empty:" ${dir[$i-1]}
-else
-    echo "Deleting:" ${dir[$i-1]}
-    rmdir ${dir[$i-1]}
-fi
+    if [ -f ${dir[$i-1]}/.DS_Store ]
+    then
+        rm ${dir[$i-1]}/.DS_Store
+    fi
+    if [ "$(ls -A ${dir[$i-1]})" ]
+    then
+        echo "Not Empty:" ${dir[$i-1]}
+    else
+        echo "Deleting:" ${dir[$i-1]}
+        rmdir ${dir[$i-1]}
+    fi
 done
