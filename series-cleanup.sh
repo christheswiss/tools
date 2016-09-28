6897439bc5ed68846724a4dc0fac20162f0588f2
christheswiss <chris.luscher@gmx.de>
2016-09-28 11:26:58 +0200

    handles empty series dir

--- series-cleanup.sh
+++ series-cleanup.sh
@@ -4,7 +4,12 @@ dir=()
 
 for j in /Volumes/dragon/series/*
 do
-    dir+=($j)
+    if [ "$j" == "/Volumes/dragon/series/*" ]
+    then
+        echo "No directories"
+    else
+        dir+=($j)
+    fi
     for k in "$j"/*
     do
         if [ -d $k ]
@@ -16,17 +21,21 @@ done
 
 dirnum=${#dir[@]}
 
-for i in $(seq $dirnum 1)
-do
-    if [ -f ${dir[$i-1]}/.DS_Store ]
-    then
-        rm ${dir[$i-1]}/.DS_Store
-    fi
-    if [ "$(ls -A ${dir[$i-1]})" ]
-    then
-        echo "Not Empty:" ${dir[$i-1]}
-    else
-        echo "Deleting:" ${dir[$i-1]}
-        rmdir ${dir[$i-1]}
-    fi
-done
+if ( test $dirnum -gt "0" )
+then
+    for i in $(seq $dirnum 1)
+    do
+        if [ -f ${dir[$i-1]}/.DS_Store ]
+        then
+            echo "Deleting:" ${dir[$i-1]}/.DS_Store
+            rm ${dir[$i-1]}/.DS_Store
+        fi
+        if [ "$(ls -A ${dir[$i-1]})" ]
+        then
+            echo "Not Empty:" ${dir[$i-1]}
+        else
+            echo "Deleting:" ${dir[$i-1]}
+            rmdir ${dir[$i-1]}
+        fi
+    done
+fi
