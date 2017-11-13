89b881a94e2eec2336a26f1467364abd666e01da
Christoph M. Lüscher <chris.luscher@gmx.de>
2017-11-13 10:16:18 +0100

    nicer output

--- brewoutdated.sh
+++ brewoutdated.sh
@@ -4,5 +4,5 @@ updated=false;
 /usr/local/bin/brew update && updated=true;
 test ${updated} != true && echo "Update failed!" && /usr/local/bin/terminal-notifier -title "Homebrew" -message "Update failed!";
 outdated="$(/usr/local/bin/brew outdated)";
-test ${updated} == true && test "${outdated}" != "" && echo "==> Outdated Formulae" && echo "${outdated}" && /usr/local/bin/terminal-notifier -title "Homebrew" -message "Packages require update: ${outdated}";
+test ${updated} == true && test "${outdated}" != "" && echo "==> Outdated Formulae" && echo "${outdated}" && /usr/local/bin/terminal-notifier -title "Homebrew" -message $'Packages require update:\n'${outdated};
 test ${updated} == true && test "${outdated}" == "" && /usr/local/bin/terminal-notifier -title "Homebrew" -message "No update required";
